0f934ac6a1f2bf892ed7a7d5fdb204525dd8273a
Mariusz Derda <mariusz.derda@gmail.com>
2021-01-05 17:02:26 +0100

    Config prettier, eslint, husky, lint-staged

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,7 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          <code>src/App.js</code>
+          and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +18,17 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda beatae culpa earum,
+          esse exercitationem fugiat ipsam quidem unde. Alias amet aut consectetur cum cumque,
+          debitis dolores error exercitationem facilis fuga fugiat harum hic ipsam iste magni nihil
+          non, nulla numquam odit officiis pariatur perspiciatis possimus praesentium, quae quaerat
+          quibusdam quidem saepe sed similique unde? A beatae, culpa cupiditate dicta doloremque
+          dolorum ea hic illum iste laboriosam necessitatibus placeat quod, repudiandae. Amet
+          aperiam assumenda beatae eveniet officia qui, reprehenderit! Animi dolor est et ex
+          incidunt, maxime molestiae obcaecati perferendis perspiciatis, ratione repellat
+          repellendus tempore temporibus! Accusantium amet asperiores atque commodi dolore
+          doloremque eligendi, exercitationem expedita fuga fugit id magnam minus molestias, neque
+          perferendis perspiciatis provident quaerat quam quasi, vel veritatis voluptatum.
         </a>
       </header>
     </div>
